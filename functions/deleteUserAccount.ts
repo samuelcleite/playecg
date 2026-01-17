ce3e335a22ea05ded33432308713cebeb3f3c995
Descomplica ECG <ecgdescomplica@gmail.com>
2026-01-17 12:23:19 +0000

    File changes

--- functions/deleteUserAccount.ts
+++ functions/deleteUserAccount.ts
@@ -12,13 +12,28 @@ Deno.serve(async (req) => {
         const userEmail = user.email;
 
         // Delete all user data
-        await base44.asServiceRole.entities.QuizAttempt.delete({ user_email: userEmail });
-        await base44.asServiceRole.entities.DailyQuizStats.delete({ user_email: userEmail });
-        await base44.asServiceRole.entities.Payment.delete({ user_email: userEmail });
-        await base44.asServiceRole.entities.CouponUsage.delete({ user_email: userEmail });
+        const quizAttempts = await base44.asServiceRole.entities.QuizAttempt.filter({ user_email: userEmail });
+        for (const attempt of quizAttempts) {
+            await base44.asServiceRole.entities.QuizAttempt.delete(attempt.id);
+        }
+
+        const dailyStats = await base44.asServiceRole.entities.DailyQuizStats.filter({ user_email: userEmail });
+        for (const stat of dailyStats) {
+            await base44.asServiceRole.entities.DailyQuizStats.delete(stat.id);
+        }
+
+        const payments = await base44.asServiceRole.entities.Payment.filter({ user_email: userEmail });
+        for (const payment of payments) {
+            await base44.asServiceRole.entities.Payment.delete(payment.id);
+        }
+
+        const couponUsages = await base44.asServiceRole.entities.CouponUsage.filter({ user_email: userEmail });
+        for (const usage of couponUsages) {
+            await base44.asServiceRole.entities.CouponUsage.delete(usage.id);
+        }
 
         // Delete the user account itself
-        await base44.asServiceRole.entities.User.delete({ email: userEmail });
+        await base44.asServiceRole.entities.User.delete(user.id);
 
         return Response.json({ success: true });
     } catch (error) {
